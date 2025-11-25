8ecb97cb4481460722648718adf58c866949c866
Joao Piologo <joaotpiologo@hotmail.com>
2025-11-24 21:19:43 -0300

    Patch: Validations

--- src/main/java/br/com/devpiologo/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/devpiologo/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,14 +1,24 @@
 package br.com.devpiologo.gestao_vagas.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
 import java.util.UUID;
 
 @Data
 public class CandidateEntity {
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo não deve conter caracteres especiais.")
     private String username;
+
+    @Email(message = "Must be a valid e-mail.")
     private String email;
+
+    @Length(min = 10, max = 50)
     private String password;
     private String description;
     private String[] skills;
