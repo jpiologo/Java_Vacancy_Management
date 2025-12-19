63bc5669e8a6d7fdce4bcbb260936b0dd7445708
joao.piologo <joao.piologo@grupobarigui.com.br>
2025-12-19 13:04:42 -0300

    patch: error message enhancement

--- src/main/java/br/com/devpiologo/gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/devpiologo/gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -1,7 +1,12 @@
 package br.com.devpiologo.gestao_vagas.modules.candidate.controller;
 
+import br.com.devpiologo.gestao_vagas.exceptions.UserFoundException;
 import br.com.devpiologo.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.devpiologo.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.devpiologo.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private CreateCandidateUseCase createCandidateUseCase;
+
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidateEntity){
-        System.out.println("Candidato: " + candidateEntity.getEmail());
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity){
+        try{
+            var result = this.createCandidateUseCase.execute(candidateEntity);
+            return ResponseEntity.ok().body(result);
+        }catch(Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
