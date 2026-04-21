feb8cb155d01d356d2034f0d1df8369988282044
João Piologo <joaotpiologo@hotmail.com>
2026-04-21 16:30:42 -0300

    ✨feat: Autenticação e Autorização - Aula 4
    Validar credenciais e retornar JWT ou mensagem de erro

--- src/main/java/br/com/devpiologo/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/devpiologo/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -2,8 +2,12 @@ package br.com.devpiologo.gestao_vagas.modules.company.useCases;
 
 import br.com.devpiologo.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 import br.com.devpiologo.gestao_vagas.modules.company.repositories.CompanyRepository;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,16 +15,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthCompanyUseCase {
 
+    @Value("${security.token.secret}")
+    private String secretKey;
+
     @Autowired
     private CompanyRepository companyRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public void execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
         var company = companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
                 () -> {
-                    throw new UsernameNotFoundException("Company not found");
+                    throw new UsernameNotFoundException("Invalid username or password");
                 }
         );
 
@@ -31,5 +38,13 @@ public class AuthCompanyUseCase {
         if(!passwordMatches) {
             throw new AuthenticationException();
         }
+
+        //Se as senhas baterem, gera o JWT
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var token = JWT.create().withIssuer("javagas")
+                .withSubject(company.getId().toString())
+                .sign(algorithm);
+
+        return token;
     }
 }
